6d40b075ba1b735caa22b9290e8285a0308d070a
VaishnaviMahajan <vaishnavikmahajan090308@gmail.com>
2024-01-16 21:39:19 +0530

    done

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-document.querySelector('.menu-icon').addEventListener('click', function () {
-    var links = document.querySelector('.responsive-links');
-    links.style.display = (links.style.display === 'block' ? 'none' : 'block');
+document.querySelector(".menu-icon").addEventListener("click", function () {
+  var links = document.querySelector(".responsive-links");
+  links.style.display = links.style.display === "block" ? "none" : "block";
 });
-
-// JavaScript to dynamically update the current year
-document.getElementById("currentYear").innerText =
-new Date().getFullYear();
+document.getElementById("currentYear").innerText = new Date().getFullYear();
